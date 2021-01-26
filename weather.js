e5f4b1e566d29adf402ae732b88a476824203d6e
Toni Powell <tonipow3ll@gmail.com>
2021-01-26 16:03:12 -0500

    basics laid out

--- weather.js
+++ weather.js
@@ -2,29 +2,29 @@ $( document ).ready(function() {
 
     // full link 
     //https://api.openweathermap.org/data/2.5/weather?q= LOCATIONHERE&units=imperial&appid=49f44633a22b64ed242a4f937e6ef855
+    
+    //weather API for current city 
     const api = {
         key: "6e4d2dd0c20c78bde4e20e01e275ea85",
         base: "https://api.openweathermap.org/data/2.5/"
-       // base: "https://api.openweathermap.org/data/2.5/"
-       // original API URL  base: "https://api.openweathermap.org/data/2.5/"
      }
-//weather API
-// let queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}" ;
-// let APIKey = "&appid=49f44633a22b64ed242a4f937e6ef855";
-// let units = "&units=imperial"
+     // weather API for five day 
+    const fivedayApi = {
+        fiveKey: "ed7b87dda4d1465a2d3cfe935f385dd8",
+        fiveBase: "https://api.openweathermap.org/data/2.5/"
+    }
+
 const searchBox = document.querySelector('.search-box');
 const lat = document.querySelector('.lat');
 const lon = document.querySelector('.lon');
 
- const fivedayApi = {
-     fiveKey: "3fa7210f886680035b976ee7d2b85be0",
-     fiveBase: "https://api.openweathermap.org/data/2.5/"
- }
+//event listener / button for FIVE DAY weather
 $('.future').on('click', function(){
     console.log(lat.value)
     console.log(lon.value)
     getFiveDay(lat.value, lon.value);
 })
+// API call for FIVE DAY weather
   function getFiveDay (lat, lon){
     fetch(`${fivedayApi.fiveBase}onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${fivedayApi.fiveKey}`)
     .then(response => {
@@ -32,24 +32,36 @@ $('.future').on('click', function(){
     }).then(showForecast)
   };
 
+  // function to show FIVE DAY weather
 function showForecast(response){
     console.log(response);
+    let UVI = document.querySelector('.fiveDay .UVI1');
+    let futureDescription = document.querySelector('.fiveDay .futureCastDescription1');
+    let future1 = document.querySelector('.fiveDay .futureCastTemp1')
+    let locationIcon = document.querySelector('.fiveDay .weather-icon');
+    const {icon} = `${response.daily[0].weather[0].icon}`;
+    UVI.innerHTML = `${response.current.uvi}<span> UVI</span>`;
+    futureDescription.innerText = `${response.daily[0].weather[0].description}`;
+    future.innerText = `${response.daily[0].temp.day}<span> °F</span>`
+    locationIcon.innerHTML =`<img src="icons/${icon}.png"></img>`
 }
 
+// event listener / button for CURRENT weather
 $('.locationsearch').on('click', function (){
     console.log(searchBox.value)
     getWeather(searchBox.value)
 })
 
+// function for getting CURRENT weather
 function getWeather(query){
     fetch(`${api.base}forecast?q=${query}&units=imperial&APPID=${api.key}`)
     .then(response => {
         return response.json();
     }).then(showWeather)
 };
-
+// Function to show CURRENT weather only 
+console.log(response);
 function showWeather(response){
-    console.log(response);
     let city = document.querySelector('.location .city')
     city.innerText = `${response.city.name}, ${response.city.country}`;
     
@@ -61,6 +73,11 @@ function showWeather(response){
 
     let currentWeather = document.querySelector('.currentWeather')
     currentWeather.innerText = `${response.list[0].weather[0].description}`
+
+    let icon = document.querySelector('.icon')
+    const {icon1} = `${response.list[0].weather[0].icon}`
+    // currentWeather.innerText = `${response.list[0].weather[0].icon}`
+    icon.innerHTML =`<img alt ="weather icon" src="${icon1}.png"></img>`
 };
 
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
