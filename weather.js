2eef9917386c1f9da1bf9ff6f115b1e9c36e6c75
Toni Powell <tonipow3ll@gmail.com>
2021-01-26 18:34:46 -0500

    five day forecast works

--- weather.js
+++ weather.js
@@ -36,14 +36,13 @@ $('.future').on('click', function(){
   // function to show FIVE DAY weather
 function showForecast(response){
     console.log(response);
+    // day 1
     let UVI = document.querySelector('.fiveDay .UVI1');
     let futureDescription = document.querySelector('.fiveDay .futureCastDescription1');
     let future1 = document.querySelector('.fiveDay .futureCastTemp1');
     let locationIcon = document.querySelector('.fiveDay .weather-icon1');
     let humidity1 = document.querySelector('.futureCastHumidity1');
     let wind1 = document.querySelector('.futureWind1')
-
-    
     let icon = `${response.daily[0].weather[0].icon}`;
     UVI.innerHTML = `${response.current.uvi}<span> UVI</span>`;
     futureDescription.innerText = `${response.daily[0].weather[0].description}`;
@@ -51,6 +50,67 @@ function showForecast(response){
     humidity1.innerHTML =`${response.daily[0].humidity}<span> % Humidity</span>`;
     wind1.innerHTML = `${response.daily[0].wind_speed}<span> Wind Speed</span>`;
     locationIcon.innerHTML =`<img src="http://openweathermap.org/img/wn/${icon}.png"></img>`;
+
+    // day 2
+    let UVI2 = document.querySelector('.fiveDay .UVI2');
+    let futureDescription2 = document.querySelector('.fiveDay .futureCastDescription2');
+    let future2 = document.querySelector('.fiveDay .futureCastTemp2');
+    let locationIcon2 = document.querySelector('.fiveDay .weather-icon2');
+    let humidity2 = document.querySelector('.futureCastHumidity2');
+    let wind2 = document.querySelector('.futureWind2')
+    let icon2 = `${response.daily[1].weather[0].icon}`;
+    UVI2.innerHTML = `${response.current.uvi}<span> UVI</span>`;
+    futureDescription2.innerText = `${response.daily[1].weather[0].description}`;
+    future2.innerHTML = `${response.daily[1].temp.day}<span> °F</span>`;
+    humidity2.innerHTML =`${response.daily[1].humidity}<span> % Humidity</span>`;
+    wind2.innerHTML = `${response.daily[1].wind_speed}<span> Wind Speed</span>`;
+    locationIcon2.innerHTML =`<img src="http://openweathermap.org/img/wn/${icon2}.png"></img>`;
+    
+    // day 3
+    let UVI3 = document.querySelector('.fiveDay .UVI3');
+    let futureDescription3 = document.querySelector('.fiveDay .futureCastDescription3');
+    let future3 = document.querySelector('.fiveDay .futureCastTemp3');
+    let locationIcon3 = document.querySelector('.fiveDay .weather-icon3');
+    let humidity3 = document.querySelector('.futureCastHumidity3');
+    let wind3 = document.querySelector('.futureWind3')
+    let icon3 = `${response.daily[2].weather[0].icon}`;
+    UVI3.innerHTML = `${response.current.uvi}<span> UVI</span>`;
+    futureDescription3.innerText = `${response.daily[2].weather[0].description}`;
+    future3.innerHTML = `${response.daily[2].temp.day}<span> °F</span>`;
+    humidity3.innerHTML =`${response.daily[2].humidity}<span> % Humidity</span>`;
+    wind3.innerHTML = `${response.daily[2].wind_speed}<span> Wind Speed</span>`;
+    locationIcon3.innerHTML =`<img src="http://openweathermap.org/img/wn/${icon3}.png"></img>`;
+
+    // day 4
+
+    let UVI4 = document.querySelector('.fiveDay .UVI4');
+    let futureDescription4 = document.querySelector('.fiveDay .futureCastDescription4');
+    let future4 = document.querySelector('.fiveDay .futureCastTemp4');
+    let locationIcon4 = document.querySelector('.fiveDay .weather-icon4');
+    let humidity4 = document.querySelector('.futureCastHumidity4');
+    let wind4 = document.querySelector('.futureWind1')
+    let icon4 = `${response.daily[3].weather[0].icon}`;
+    UVI4.innerHTML = `${response.current.uvi}<span> UVI</span>`;
+    futureDescription4.innerText = `${response.daily[3].weather[0].description}`;
+    future4.innerHTML = `${response.daily[3].temp.day}<span> °F</span>`;
+    humidity4.innerHTML =`${response.daily[3].humidity}<span> % Humidity</span>`;
+    wind4.innerHTML = `${response.daily[3].wind_speed}<span> Wind Speed</span>`;
+    locationIcon4.innerHTML =`<img src="http://openweathermap.org/img/wn/${icon4}.png"></img>`;
+
+    // day 5
+    let UVI5 = document.querySelector('.fiveDay .UVI5');
+    let futureDescription5 = document.querySelector('.fiveDay .futureCastDescription5');
+    let future5 = document.querySelector('.fiveDay .futureCastTemp5');
+    let locationIcon5 = document.querySelector('.fiveDay .weather-icon5');
+    let humidity5 = document.querySelector('.futureCastHumidity5');
+    let wind5 = document.querySelector('.futureWind5')
+    let icon5 = `${response.daily[4].weather[0].icon}`;
+    UVI5.innerHTML = `${response.current.uvi}<span> UVI</span>`;
+    futureDescription5.innerText = `${response.daily[4].weather[0].description}`;
+    future5.innerHTML = `${response.daily[4].temp.day}<span> °F</span>`;
+    humidity5.innerHTML =`${response.daily[4].humidity}<span> % Humidity</span>`;
+    wind5.innerHTML = `${response.daily[4].wind_speed}<span> Wind Speed</span>`;
+    locationIcon5.innerHTML =`<img src="http://openweathermap.org/img/wn/${icon5}.png"></img>`;
 }
 
 // event listener / button for CURRENT weather
