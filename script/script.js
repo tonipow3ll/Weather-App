b05b6da49fc478f8b8f5dc36170e13cede40e916
Toni Powell <tonipow3ll@gmail.com>
2021-04-22 16:57:41 -0400

    getting there

--- script/script.js
+++ script/script.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() { 
 console.log("ready!")
 
+const fiveDay = [];
+
 // local storage - get previous searched items if they exist
 let searchData;
 if (!JSON.parse(localStorage.getItem('history'))) {
@@ -35,44 +37,89 @@ $('.day4').text(fifthDay);
 
 
 // on load asks for users location, will populate with weather data if allowed
-const getLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
-    } 
-    else {
-      alert("Geolocation is not supported by this browser.");
-    }
-  };
+// const getLocation = () => {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(showPosition);
+//     } 
+//     else {
+//       alert("Geolocation is not supported by this browser.");
+//     }
+//   };
 
-  function showPosition(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    redirect(lat, lon);
-  };
+//   function showPosition(position) {
+//     const lat = position.coords.latitude;
+//     const lon = position.coords.longitude;
+//     redirect(lat, lon);
+//   };
 
 $('#search-btn').click((event) => {
     // query might break 
     event.preventDefault();
+    getWeather();
 const query = $('#search').val().trim();
-let currentCity = `${api.base}forecast?q=${query}&units=imperial&APPID=cad650368f646db27b36a263f6e8c5df`
+// let currentCity = `${api.base}forecast?q=${query}&units=imperial&APPID=cad650368f646db27b36a263f6e8c5df`
 
-$.ajax({
-    url: currentCity,
-    method: "GET"
-}).then((response) => {
-  console.log(response)
-  // let city = $('#location');
-  // city.innerHTML = `${response.city.name}`
-    $('#location').text(response.city.name)
-    $('#currentTemp').text("Current Temp " + Math.round(response.list[0].main.temp) + '°F')
-    $('#lo').text('Low ' + Math.round(response.list[0].main.temp_min) + '°F')
-    $('#hi').text('High ' + Math.round(response.list[0].main.temp_min) + '°F')
-    $('#humidity').text('Humidity ' + response.list[0].main.humidity)
-    $('#wind').text('Wind Speed ' + response.list[0].wind.speed)
-    })
 })
 
-  getLocation();
+getWeather = () => {
+  const query = $('#search').val().trim();
+  let currentCity = `${api.base}forecast?q=${query}&units=imperial&APPID=cad650368f646db27b36a263f6e8c5df`
+  $.get(currentCity).then((response) => {
+    // console.log(response)
+  
+    // const lat = response.city.coord.lat;
+    // const lon = response.city.coord.lon;
+    // let city = $('#location');
+    // city.innerHTML = `${response.city.name}`
+      let fiveDayWeather = $('<div class="container-fluid">')
+      $('#location').text(response.city.name)
+      $('#currentTemp').text("Current Temp " + Math.round(response.list[0].main.temp) + '°F')
+      $('#lo').text('Low ' + Math.round(response.list[0].main.temp_min) + '°F')
+      $('#hi').text('High ' + Math.round(response.list[0].main.temp_min) + '°F')
+      $('#humidity').text('Humidity ' + response.list[0].main.humidity + '%')
+      $('#wind').text('Wind Speed ' + response.list[0].wind.speed + 'mph')
+      $('#desc').text(response.list[0].weather[0].description)
+      $('#icon').attr("src","http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png")
+    
+    })
+    .then(() => {
+      $.get(currentCity).then((response) => {
+        console.log(response)
+        let dayone = response.list[1];
+        let daytwo = response.list[2];
+        let daythree = response.list[2];
+        let dayfour = response.list[2];
+        let dayfive = response.list[2];
+        if (fiveDay.length > 0){
+          fiveDay.length = 0;
+          // console.log(fiveDay)
+        }
+        fiveDay.push(dayone, daytwo, daythree, dayfour, dayfive)
+    
+        // console.log(fiveDay);
+        writeForecast()
+      
+      })
+  
+    })
+}
+
+ writeForecast = () => {
+  console.log(fiveDay)
+  $('#icon1').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
+  $('#icon2').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
+  $('#icon3').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
+  $('#icon4').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
+  $('#icon5').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
+  $('#1').text('Temp ' + Math.round(fiveDay[0].main.temp) + '°F')
+  $('#2').text('Temp ' + Math.round(fiveDay[1].main.temp) + '°F')
+  $('#3').text('Temp ' + Math.round(fiveDay[2].main.temp) + '°F')
+  $('#4').text('Temp ' + Math.round(fiveDay[3].main.temp) + '°F')
+  $('#5').text('Temp ' + Math.round(fiveDay[4].main.temp) + '°F')
+ }
+
+
+  // getLocation();
 // END SCRIPT
 });
 
