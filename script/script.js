ad056e00c7cfb0feb766a5426ab9cd40efa297f2
Toni Powell <tonipow3ll@gmail.com>
2021-04-22 18:45:32 -0400

    stuff

--- script/script.js
+++ script/script.js
@@ -4,12 +4,12 @@ console.log("ready!")
 const fiveDay = [];
 
 // local storage - get previous searched items if they exist
-let searchData;
-if (!JSON.parse(localStorage.getItem('history'))) {
-    searchData = [];
-} else {
-    searchData = JSON.parse(localStorage.getItem('history'));
-}
+// let searchData;
+// if (!JSON.parse(localStorage.getItem('history'))) {
+//     searchData = [];
+// } else {
+//     searchData = JSON.parse(localStorage.getItem('history'));
+// }
 // api for local weather, might not need both of these
 const api = {
    key: "cad650368f646db27b36a263f6e8c5df",
@@ -28,12 +28,14 @@ const nextDay = dayjs().add(1,'day').format('dddd');
 const thirdDay = dayjs().add(2,'day').format('dddd');
 const fourthDay = dayjs().add(3,'day').format('dddd');
 const fifthDay = dayjs().add(4,'day').format('dddd');
+const sixthDay = dayjs().add(5,'day').format('dddd');
 $(".date").text(currentDate);
 $('.day').text(currentDay);
 $('.day1').text(nextDay);
 $('.day2').text(thirdDay);
 $('.day3').text(fourthDay);
 $('.day4').text(fifthDay);
+$('.day5').text(sixthDay);
 
 
 // on load asks for users location, will populate with weather data if allowed
@@ -56,9 +58,16 @@ $('#search-btn').click((event) => {
     // query might break 
     event.preventDefault();
     getWeather();
-const query = $('#search').val().trim();
-// let currentCity = `${api.base}forecast?q=${query}&units=imperial&APPID=cad650368f646db27b36a263f6e8c5df`
+    const query = $('#search').val().trim();
 
+    let cities = JSON.parse(localStorage.getItem("storedSearchHistory")) || [];
+    cities.push(query);
+
+    localStorage.setItem("storedSearchHistory", JSON.stringify(cities));
+
+    // Print list of cities to the page
+    //  cities.forEach($(".cities").prepend('<li>' + cities + '</li>'));
+   
 })
 
 getWeather = () => {
@@ -71,7 +80,7 @@ getWeather = () => {
     // const lon = response.city.coord.lon;
     // let city = $('#location');
     // city.innerHTML = `${response.city.name}`
-      let fiveDayWeather = $('<div class="container-fluid">')
+
       $('#location').text(response.city.name)
       $('#currentTemp').text("Current Temp " + Math.round(response.list[0].main.temp) + '°F')
       $('#lo').text('Low ' + Math.round(response.list[0].main.temp_min) + '°F')
@@ -84,7 +93,7 @@ getWeather = () => {
     })
     .then(() => {
       $.get(currentCity).then((response) => {
-        console.log(response)
+        // console.log(response)
         let dayone = response.list[1];
         let daytwo = response.list[2];
         let daythree = response.list[2];
@@ -105,6 +114,7 @@ getWeather = () => {
 }
 
  writeForecast = () => {
+   $('.five-day').removeClass('d-none')
   console.log(fiveDay)
   $('#icon1').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
   $('#icon2').attr("src","http://openweathermap.org/img/wn/" + fiveDay[0].weather[0].icon + "@2x.png")
